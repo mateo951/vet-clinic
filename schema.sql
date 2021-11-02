852b710a493ed5e468a6bf2bd3f0a5b062f4e92c
Mateo Villagómez <mateovillagomez1995@gmail.com>
2021-11-02 10:48:05 -0500

    Add readme file & db schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+create table animals (
+  id serial,name varchar(45),
+  date_of_birth date,
+  escape_attemts integer,
+  neutered boolean,
+  weight_kg decimal(10,4),
+  user_name varchar (45),
+  timestamp timestamp,
+  primary key (id)
 );
