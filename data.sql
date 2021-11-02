c3a92d5e38c731ae34b27631eee35f7c7a24c37a
Mateo Villagómez <mateovillagomez1995@gmail.com>
2021-11-02 12:11:56 -0500

    Add queries to select specific segmented data

--- data.sql
+++ data.sql
@@ -1,6 +1,6 @@
 /* Populate database with sample data. */
 
-insert into animals values (default, 'Agumon 2', '20200206', 1, true, 14.23);
+insert into animals values (default, 'Agumon', '20200206', 1, true, 14.23);
 insert into animals values (default, 'Gabumon', '20181115', 2, true, 8.00);
 insert into animals values (default, 'Pikachu', '20210107', 1, false, 15.04);
 insert into animals values (default, 'Devimon', '20170512', 5, true, 11.00);
