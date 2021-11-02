786d8908ecd20be57d9e5f2acd6f3297978b8429
Mateo Villagómez <mateovillagomez1995@gmail.com>
2021-11-02 11:30:38 -0500

    Change some schema attributes & Add insert query for data

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+insert into animals values (default, 'Agumon 2', '20200206', 1, true, 14.23);
+insert into animals values (default, 'Gabumon', '20181115', 2, true, 8.00);
+insert into animals values (default, 'Pikachu', '20210107', 1, false, 15.04);
+insert into animals values (default, 'Devimon', '20170512', 5, true, 11.00);
--- schema.sql
+++ schema.sql
@@ -1,12 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 create table animals (
-  id serial,name varchar(45),
-  date_of_birth date,
-  escape_attemts integer,
-  neutered boolean,
+	id serial,
+	name varchar(45),
+	date_of_birth date,
+	escape_attempts integer,
+	neutered boolean,
   weight_kg decimal(10,4),
-  user_name varchar (45),
-  timestamp timestamp,
-  primary key (id)
+	PRIMARY KEY(id)
 );
