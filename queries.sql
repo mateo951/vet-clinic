0a6895d98091a4c696cc3418a9c06744a2e84dfc
Mateo Villagómez <mateovillagomez1995@gmail.com>
2021-11-03 22:09:28 -0500

    Add new tables, insert new data, and new queries

--- queries.sql
+++ queries.sql
@@ -36,3 +36,26 @@ select avg(weight_kg) from animals;
 select neutered, sum(escape_attempts) from animals group by neutered order by sum desc;
 select species, min(weight_kg), max(weight_kg) from animals group by species;
 select species, avg(escape_attempts) from animals where extract(year from date_of_birth) between 1990 and 2000 group by species;
+
+-- Foreign Keys queries
+
+alter table animals drop column species;
+alter table animals add species_id int, add constraint fk foreign key (species_id) references species(id);
+alter table animals add owner_id int, add constraint fk_owners foreign key (owner_id) references owner(owner_id);
+
+update animals set species_id = (select id from species where name like 'Digimon') where name like '%mon%';
+update animals set species_id = (select id from species where name like 'Pokemon') where name not like '%mon';
+
+update animals set owner_id = (select owner_id from owners where full_name = 'Sam Smith') where name like '%Agumon%';
+update animals set owner_id = (select owner_id from owners where full_name = 'Jennifer Orwell') where name like '%Gabumon%' or name like '%Pikachu%';
+update animals set owner_id = (select owner_id from owners where full_name = 'Bob') where name like '%Devimon%' or name like '%Plantmon%';
+update animals set owner_id = (select owner_id from owners where full_name = 'Melody Pond') where name like '%Charmander%' or name like '%Squirtle%' or name like '%Blossom%';
+update animals set owner_id = (select owner_id from owners where full_name = 'Dean Winchester') where name like '%Angemon%' or name like '%Boarmon%';
+
+select count(animals.owner_id) from animals inner join owners on animals.owner_id = owners.owner_id where owners.full_name like 'Melody Pond';
+select * from animals inner join species on animals.species_id = species.id where species.name like 'Pokemon';
+select owners.full_name, animals.name from animals right join owners on animals.owner_id = owners.owner_id;
+select species.name, count(species.id) from animals inner join species on animals.species_id = species.id group by species.name;
+select animals.name, species_id from animals inner join owners on animals.owner_id = owners.owner_id inner join species on animals.species_id = species.id where owners.full_name like '%Jennifer Orwell%' and species.name like 'Digimon';
+select owners.full_name, animals.name from owners inner join animals on owners.owner_id = animals.owner_id inner join species on species.id = animals.species_id where owners.full_name like '%Dean Winchester%' and animals.escape_attempts = 0;
+select owners.full_name, count(*) from owners join animals on owners.owner_id = animals.owner_id group by owners.full_name having count(*) > 2;
